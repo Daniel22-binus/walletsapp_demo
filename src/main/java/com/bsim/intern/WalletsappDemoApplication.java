b0bcc8c5ceb50d68c4339cf07332ffe3f455ce28
Daniel22-binus <daniel025@binus.ac.id>
2021-04-20 13:55:52 +0700

    initialize project

---
+++ src/main/java/com/bsim/intern/WalletsappDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bsim.intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletsappDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletsappDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsim/intern/WalletsappDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsim.intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletsappDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
